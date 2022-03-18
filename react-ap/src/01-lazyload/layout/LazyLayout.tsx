61950fb134f6ea4953afbe2fb729bbba2e44530a
Javier Lucero <javier.lucero@iess.gob.ec>
2022-03-18 12:38:39 -0500

    rutas anidadas

--- react-ap/src/01-lazyload/layout/LazyLayout.tsx
+++ react-ap/src/01-lazyload/layout/LazyLayout.tsx
@@ -21,6 +21,8 @@ export const LazyLayout = () => {
             <Route path="lazy1" element={<LazyPage1/>}/>
             <Route path="lazy2" element={<LazyPage2/>}/>
             <Route path="lazy3" element={<LazyPage3/>}/>
+            {/* <Route path="*" element={<div>Not found</div>}/> */}
+            <Route path="*" element={<Navigate replace to="lazy1" />}/>
         </Routes>
         </div>
   )
