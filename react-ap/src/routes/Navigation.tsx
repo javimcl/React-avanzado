813aca85722cd1864badd9eb379c82470c992da4
Javier Lucero <javier.lucero@iess.gob.ec>
2022-03-18 12:24:26 -0500

    commit

--- react-ap/src/routes/Navigation.tsx
+++ react-ap/src/routes/Navigation.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter, NavLink } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
 
 import logo from '../logo.svg'
 
